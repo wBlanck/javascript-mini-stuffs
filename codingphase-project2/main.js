2b9bb7c68886668368b46c8a92ee6b61e8ef20b3
wblanck <blanck.william@gmail.com>
2021-08-26 08:49:38 +0200

    added comments and pseudo code

--- codingphase-project2/main.js
+++ codingphase-project2/main.js
@@ -1,3 +1,5 @@
+
+// this is the database
 var pokemonDB = [
   {
     name: 'charmander',
@@ -29,55 +31,80 @@ var pokemonDB = [
   
 ]
 
-
+// state
 let gameState = {
   userPokemon: '',
   rivalPokemon: ''
 }
-
+// elements
 let pokemonsEl = document.querySelector('.select-screen').querySelectorAll('.character');
-
 let battleScreenEl = document.getElementById('battle-screen')
 
+let i = 0;
 
-i = 0;
-
+// initial loop
 while(i < pokemonsEl.length){
+  //add function to all characters on screen select
+
   pokemonsEl[i].onclick = function () {
+    // current selected pokemon name
     let pokemonName = this.dataset.pokemon;
+    // elements for the name and images
     let player1Img = document.querySelector('.player1').getElementsByTagName('img');
     let player1Name = document.querySelector('.player1 .stats .name');
     let player2Img = document.querySelector('.player2').getElementsByTagName('img');
     let player2Name = document.querySelector('.player2 .stats .name');
 
+    // save the current pokemon
     gameState.userPokemon = pokemonName;
   
+    // cpu pics a pokemon
     cpuPick();
+    // change screen to battle scene
     battleScreenEl.classList.toggle('active')
 
+    // select data from current user pokemon
     let currentPokemon = pokemonDB.filter(function(pokemon){
       return pokemon.name === gameState.userPokemon;
     })
-
+    player1Img[0].src = currentPokemon[0].img;
+    player1Name.innerText = gameState.userPokemon;
+    // select data from current cpu pokemon
     let currentRivalPokemon = pokemonDB.filter(function(pokemon){
       return pokemon.name === gameState.rivalPokemon;
     })
-    
-    
-    player1Img[0].src = currentPokemon[0].img;
-    player1Name.innerText = gameState.userPokemon;
     player2Img[0].src = currentRivalPokemon[0].img;
     player2Name.innerText = gameState.rivalPokemon;
+    
+    // user choose attack
+
+    // cpu health goes down
+
+    // cpu attack
+
+    // user health goes down
+
+    
+
+    // rock > scissors
+
+    // paper > rock
+
+    // scissors > paper
+  
+    // depending on pokemon type and defense is how hard the attack is going to be and how much health it will take out
+
+    // then who ever gets to health <= 0 loses
 
   }
   i++;
 }
 
-function randomNum(min, max) {
+let randomNum = function randomNum(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function cpuPick() {
+let cpuPick = function cpuPick() {
   gameState.rivalPokemon = pokemonsEl[randomNum(0,3)].dataset.pokemon;
 }
 
